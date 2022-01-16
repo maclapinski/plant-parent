f6ea4b2b2e47da12aef6b6c974e5cc19646f698c
Mac Lapinski <lapinski.mac@gmail.com>
2022-01-16 16:43:08 -0700

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/google/callback",
+        callbackURL: "https://plant-parent-app.herokuapp.com/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = new User({
