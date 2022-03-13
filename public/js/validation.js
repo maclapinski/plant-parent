ec48cbf9fbae0b87994438f88bdcd98cb972c0b3
Mac Lapinski <lapinski.mac@gmail.com>
2022-03-13 08:04:38 -0600

    form validation update

--- public/js/validation.js
+++ public/js/validation.js
@@ -21,58 +21,63 @@ const validateEmail = (email) => {
 };
 
 const validatePassword = (password) => {
-  let passwordValid = true
+  let passwordValid = true;
   const hasEightChar = password.length;
   const hasLowerCaseLetter = password.match(/[a-z]/);
   const hasUpperCaseLetter = password.match(/[A-Z]/);
-  const hasSpecialChar = password.match(/[@$!%*?&]/);
+  const hasSpecialChar = password.match(/[@$#!%*?&]/);
   const hasNumber = password.match(/[0-9]/);
-  const eightChar = document.getElementById('eight-char');
-  const upperCase = document.getElementById('upper-case');
-  const lowerCase = document.getElementById('lower-case');
-  const specialChar = document.getElementById('special-char');
-  const oneNumber = document.getElementById('one-number');
+  const eightChar = document.getElementById("eight-char");
+  const upperCase = document.getElementById("upper-case");
+  const lowerCase = document.getElementById("lower-case");
+  const specialChar = document.getElementById("special-char");
+  const oneNumber = document.getElementById("one-number");
 
-  if (hasEightChar < 8  ) {
-    eightChar.innerHTML = '<i class="fas fa-times-circle"></i>'; passwordValid= false;
+  if (hasEightChar < 8) {
+    eightChar.innerHTML = '<i class="fas fa-times-circle"></i>';
+    passwordValid = false;
+  } else {
+    eightChar.innerHTML = '<i class="fas fa-check-circle"></i>';
   }
-  else { eightChar.innerHTML = '<i class="fas fa-check-circle"></i>' }
-  
-  if (!hasLowerCaseLetter ) {
-    lowerCase.innerHTML= '<i class="fas fa-times-circle"></i>'; passwordValid= false;
-  }
-  else { lowerCase.innerHTML = '<i class="fas fa-check-circle"></i>' }
 
-  if (!hasUpperCaseLetter ) {
-    upperCase.innerHTML= '<i class="fas fa-times-circle"></i>'; passwordValid= false;
+  if (!hasLowerCaseLetter) {
+    lowerCase.innerHTML = '<i class="fas fa-times-circle"></i>';
+    passwordValid = false;
+  } else {
+    lowerCase.innerHTML = '<i class="fas fa-check-circle"></i>';
   }
-  else { upperCase.innerHTML = '<i class="fas fa-check-circle"></i>' }
 
-  if (!hasSpecialChar ) {
-    specialChar.innerHTML= '<i class="fas fa-times-circle"></i>'; passwordValid= false;
+  if (!hasUpperCaseLetter) {
+    upperCase.innerHTML = '<i class="fas fa-times-circle"></i>';
+    passwordValid = false;
+  } else {
+    upperCase.innerHTML = '<i class="fas fa-check-circle"></i>';
   }
-  else { specialChar.innerHTML = '<i class="fas fa-check-circle"></i>' }
 
-  if (!hasNumber ) {
-    oneNumber.innerHTML= '<i class="fas fa-times-circle"></i>'; passwordValid= false;
+  if (!hasSpecialChar) {
+    specialChar.innerHTML = '<i class="fas fa-times-circle"></i>';
+    passwordValid = false;
+  } else {
+    specialChar.innerHTML = '<i class="fas fa-check-circle"></i>';
   }
-  else { oneNumber.innerHTML = '<i class="fas fa-check-circle"></i>' }
-  return passwordValid
-};
 
-const validateConfirmPassword = () => {
-  return password.value === confirmPassword.value;
+  if (!hasNumber) {
+    oneNumber.innerHTML = '<i class="fas fa-times-circle"></i>';
+    passwordValid = false;
+  } else {
+    oneNumber.innerHTML = '<i class="fas fa-check-circle"></i>';
+  }
+  return passwordValid;
 };
 
 const validateName = (input) => {
   console.log(input.match(/[a-zA-Z]*/));
   console.log(input.match(/[a-zA-Z]*/)[0]);
-  if (input.match(/[a-zA-Z]*/)[0].length > 1 && !input.match(/[@$!%*?&]/) && !input.match(/[0-9]/)) {
+  if (input.match(/[a-zA-Z]*/)[0].length > 1 && !input.match(/[@$#!%*?&]/) && !input.match(/[0-9]/)) {
     return true;
   }
   return false;
 };
-const setInputChange = (inputName) => {};
 
 const setValidationPass = (element) => {
   document.querySelector(`.${element}`).classList.remove("fail");
@@ -85,9 +90,7 @@ const setValidationFail = (element, errorMessage) => {
   document.querySelector(`.${element}`).classList.add("fail");
   document.querySelector(`.${element}-pass`).removeAttribute("style");
   document.querySelector(`.${element}-fail`).style.display = "block";
-  document.querySelector(
-    `.${element}-popover`
-  ).innerHTML = `<div class='popover__content'>${errorMessage}</div>`;
+  document.querySelector(`.${element}-popover`).innerHTML = `<div class='popover__content'>${errorMessage}</div>`;
   document.querySelector(`.${element}-popover`).classList.add("slidedown");
   eval(`${element}Validated = true`);
 };
@@ -102,22 +105,14 @@ const validate = () => {
     }
   }
 
-  if (
-    (!confirmPasswordValidated && confirmPassword.value !== "") ||
-    (confirmPasswordEdited && !passwordValidated && password.value !== "")
-  ) {
-    if (validateConfirmPassword()) {
-      setValidationPass("confirmPassword");
-    } else {
-      setValidationFail("confirmPassword", `<p>Passwords don't match.</p>`);
-    }
-  }
-
   if (!lastNameValidated && lastName.value !== "") {
     if (validateName(lastName.value)) {
       setValidationPass("lastName");
     } else {
-      setValidationFail("lastName", "<p>Last name must be at least 2 characters long and must consist of letters only.</p>");
+      setValidationFail(
+        "lastName",
+        "<p>Last name must be at least 2 characters long and must consist of letters only.</p>"
+      );
     }
   }
 
@@ -125,7 +120,10 @@ const validate = () => {
     if (validateName(firstName.value)) {
       setValidationPass("firstName");
     } else {
-      setValidationFail("firstName", "<p>First name must be at least 2 characters long and must consist of letters only.</p>");
+      setValidationFail(
+        "firstName",
+        "<p>First name must be at least 2 characters long and must consist of letters only.</p>"
+      );
     }
   }
 };
@@ -137,20 +135,30 @@ const inputEventHandler = (event) => {
     confirmPasswordEdited = true;
   }
 
-  if (!passwordValidated) {
-    const passwordValidation = validatePassword(password.value);
-    
-    if (passwordValidation) {
-      setValidationPass("password");
-    } else {
-      setValidationFail("password", '');
-    }
-  }
-
   eval(`${event.currentTarget.name}Validated = false`);
 
   document.querySelector(`.${editedInput}-pass`).removeAttribute("style");
   document.querySelector(`.${editedInput}-fail`).removeAttribute("style");
+
+  if (
+    (!confirmPasswordValidated && confirmPassword.value !== "") ||
+    (confirmPasswordEdited && !passwordValidated && password.value !== "")
+  ) {
+    if (password.value === confirmPassword.value) {
+      setValidationPass("confirmPassword");
+    } else {
+      setValidationFail("confirmPassword", `<p>Passwords don't match.</p>`);
+    }
+  }
+
+  if (!passwordValidated) {
+    const passwordValidation = validatePassword(password.value);
+    if (passwordValidation) {
+      setValidationPass("password");
+    } else {
+      setValidationFail("password", "");
+    }
+  }
 
   if (editedInput === event.currentTarget.name) {
     clearTimeout(typingTimer);
